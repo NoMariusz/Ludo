d34b6874587e850c07a8f418cadfe7da29080802
NoMariusz <mariusz2727@vp.pl>
2021-03-08 21:11:33 +0100

    Turns now automatic change when worker.php run

--- api/game/gameManager.php
+++ api/game/gameManager.php
@@ -2,7 +2,6 @@
 /* Is a util class so not include anything but is used at includes.php and has
 included everything */
 
-
 function get_game_players($game_id){
     return make_querry("SELECT * FROM players WHERE game_id = $game_id");
 }
@@ -15,9 +14,6 @@ function start_game($game_id){
     $idx = random_int(0, count($players)-1);
     $active_id = $players[$idx]["id"];
     set_player_active($active_id, $game_id);
-    // start interval changing players
-    global $TURN_TIME;
-    global $change_active_player;
 }
 
 function set_player_active($player_id, $game_id){
@@ -26,15 +22,14 @@ function set_player_active($player_id, $game_id){
     // set active player status
     make_no_result_querry("UPDATE players SET status = 3 WHERE id = $player_id");
     // update start turn time
-    make_no_result_querry("UPDATE games SET turn_start_time = CURRENT_TIMESTAMP() WHERE game_id = $game_id");
+    make_no_result_querry("UPDATE games SET turn_start_time = CURRENT_TIMESTAMP() WHERE id = $game_id");
 }
 
-function change_active_player (){
+function change_active_player($game_id){
     /* function changing player having turn in base */
-    $game_id = $_SESSION['game_id'];
     $players = get_game_players($game_id);
     $active_player = make_querry("SELECT * from players WHERE game_id = $game_id AND status > 2;");
-    $active_idx = array_search($active_player, $players);
+    $active_idx = array_search($active_player[0], $players);
     echo "Active player idx: $active_idx";
     $active_idx ++;
     if ($active_idx >= count($players)){
@@ -43,6 +38,13 @@ function change_active_player (){
     $active_id = $players[$active_idx]['id'];
     set_player_active($active_id, $game_id);
 };
+
+function change_turn($game_id){
+    // change active player
+    change_active_player($game_id);
+    // change turn start time
+    make_no_result_querry("UPDATE games SET turn_start_time = CURRENT_TIMESTAMP() WHERE id = $game_id");
+}
 
 
 ?>
---
+++ api/worker.php
@@ -0,0 +1,31 @@
+<?php
+/* worker making jobs in interval that should be made as background */
+include("./includes.php");
+
+// ini_set('max_execution_time', '3600'); 
+
+function main(){
+    while(true){
+        loop();
+        sleep(5);
+    }
+}
+
+function loop(){
+    update_turns();
+}
+
+function update_turns(){
+    global $TURN_TIME;
+    $running_games = make_querry("SELECT * from games WHERE status = 1;");
+    foreach($running_games as $game){
+        $start_time = strtotime($game['turn_start_time']);
+        $time_pass = time() - $start_time;
+        if ($time_pass >= $TURN_TIME){
+            change_turn($game['id']);
+        } 
+    }
+}
+
+main();
+?>
