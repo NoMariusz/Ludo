42f1dd6d20c0e2c3225633520c93b83767657798
NoMariusz <mariusz2727@vp.pl>
2021-03-08 13:19:55 +0100

    Added database dump file

---
+++ ludo.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 08 Mar 2021, 13:18
+-- Wersja serwera: 10.4.17-MariaDB
+-- Wersja PHP: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `ludo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `games`
+--
+
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `free_spaces` int(11) NOT NULL DEFAULT 4,
+  `turn_start_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `games`
+--
+
+INSERT INTO `games` (`id`, `status`, `free_spaces`, `turn_start_time`) VALUES
+(1, 1, 2, '2021-03-08 12:13:16');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `players`
+--
+
+CREATE TABLE `players` (
+  `id` int(11) NOT NULL,
+  `nick` varchar(60) COLLATE utf8_polish_ci NOT NULL,
+  `color` varchar(60) COLLATE utf8_polish_ci NOT NULL DEFAULT 'grey',
+  `status` int(11) NOT NULL DEFAULT 0,
+  `game_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `players`
+--
+
+INSERT INTO `players` (`id`, `nick`, `color`, `status`, `game_id`) VALUES
+(1, 'tomas', 'blue', 1, 1),
+(2, 'kapitan', 'green', 1, 1);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `players`
+--
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game_id` (`game_id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `players`
+--
+ALTER TABLE `players`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `players`
+--
+ALTER TABLE `players`
+  ADD CONSTRAINT `players_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
