f925a0c99d5abbabaffa9d4f4d6e2c3c7dc12581
NoMariusz <mariusz2727@vp.pl>
2021-03-20 12:56:56 +0100

    Refactored worker.php

--- api/worker.php
+++ api/worker.php
@@ -7,58 +7,56 @@ ini_set('max_execution_time', '3600');
 
 function main(){
     while(true){
-        loop();
+        update_turns();
         sleep(2);
     }
 }
 
-function loop(){
-    update_turns();
-    update_turns_by_player_moves();
-}
-
 function update_turns(){
-    /* update turn if turn time end */
-    global $TURN_TIME;
+    // get running games
     $running_games = DbManager::make_querry(
         "SELECT * from games WHERE status = 1;");
+    // made operation for each running game
     foreach($running_games as $game){
-        $start_time = strtotime($game['turn_start_time']);
-        $time_pass = time() - $start_time;
-        if ($time_pass >= $TURN_TIME){
-            $turnManager = new TurnManager($game['id']);
-            $turnManager->change_turn();
-        } 
+        update_turns_by_time($game);
+        update_turns_by_player_moves($game);
     }
 }
 
-function update_turns_by_player_moves(){
+function update_turns_by_time($game){
+    /* update turn if turn time end */
+    global $TURN_TIME;
+    $start_time = strtotime($game['turn_start_time']);
+    $time_pass = time() - $start_time;
+    if ($time_pass >= $TURN_TIME){
+        $turnManager = new TurnManager($game['id']);
+        $turnManager->change_turn();
+    }
+}
+
+function update_turns_by_player_moves($game){
     /* update turn if player not have moves to made */
-    $running_games = DbManager::make_querry(
-        "SELECT * from games WHERE status = 1;");
-    foreach($running_games as $game){
-        // if in match not throwed cube then pass
-        if($game['throwed_cube'] == 0){
-            continue;
-        }
-        $points = $game['last_throw_points'];
-        $game_id = $game['id'];
-        $player = DbManager::make_querry(
-            "SELECT * FROM players WHERE game_id = $game_id AND status = 4"
-        )[0];
-        // if none player in game has status 4, so active not throw cube pass
-        // change turns
-        if ($player == null){
-            return false;
-        }
-        // check if player have move
-        if (check_if_player_have_moves($player ,$points)){
-            echo "player have moves <br>";
-        } else {
-            // if player have not any move
-            $turnManager = new TurnManager($game_id);
-            $turnManager->change_turn();
-        }
+    // if in match not throwed cube then pass
+    if($game['throwed_cube'] == 0){
+        continue;
+    }
+    $points = $game['last_throw_points'];
+    $game_id = $game['id'];
+    $player = DbManager::make_querry(
+        "SELECT * FROM players WHERE game_id = $game_id AND status = 4"
+    )[0];
+    // if none player in game has status 4, so active not throw cube pass
+    // change turns
+    if ($player == null){
+        return false;
+    }
+    // check if player have move
+    if (check_if_player_have_moves($player ,$points)){
+        echo "player have moves <br>";
+    } else {
+        // if player have not any move
+        $turnManager = new TurnManager($game_id);
+        $turnManager->change_turn();
     }
 }
 
